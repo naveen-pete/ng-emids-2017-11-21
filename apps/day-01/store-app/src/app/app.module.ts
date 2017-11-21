3853ddf75b55af8b21fcddc3e5fea2d647bf4387
Naveen Pete <naveen.shenoy.blr@gmail.com>
2017-11-21 18:41:30 +0530

    add code at the end of day 1

--- apps/day-01/store-app/src/app/app.module.ts
+++ apps/day-01/store-app/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductsComponent, ProductDetailComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
